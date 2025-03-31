432671471c441fe2a71998f0c91d61b7e91d2cca
maryaposohova <maryaposohova1202@gmail.com>
2025-03-31 21:56:54 +0500

    Add files via upload

---
+++ module_12/test_calk_lesson1/calk_11.py
@@ -0,0 +1,24 @@
+import test_calk
+
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    return a / b
+
+def add(a, b):
+    return a**2 + b**2
+
+if __name__ == '__main__':
+    print(add(100, 2))
+    print(sub(100, 2))
+    print(mul(100, 2))
+    print(div(100, 2))
+
+
